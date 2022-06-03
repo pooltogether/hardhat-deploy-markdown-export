7b89ad2457dc27f4491beea76a33fbee528c12c1
Pierrick Turelier <pierrick@turelier.com>
2022-06-03 14:58:47 -0500

    chore(lint): format files

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { generate } from './generate'
+import { generate } from './generate';
 
 // @ts-ignore
-task("deploy-markdown-export", "Checks whether an address won a draw")
-  .addParam("name", "The name of the deployment list")
-  .addParam("outputFilepath", "The file to output to", "deployments.md")
-  .addParam("npmPackageName", "The name of the npm package")
-  .addParam("githubBaseUrl", "The base URL of the Github repo")
+task('deploy-markdown-export', 'Checks whether an address won a draw')
+  .addParam('name', 'The name of the deployment list')
+  .addParam('outputFilepath', 'The file to output to', 'deployments.md')
+  .addParam('npmPackageName', 'The name of the npm package')
+  .addParam('githubBaseUrl', 'The base URL of the Github repo')
   .setAction(async (taskArgs: any) => {
-    const { name, outputFilepath, npmPackageName, githubBaseUrl } = taskArgs
-    await generate(name, outputFilepath, npmPackageName, githubBaseUrl)
+    const { name, outputFilepath, npmPackageName, githubBaseUrl } = taskArgs;
+    await generate(name, outputFilepath, npmPackageName, githubBaseUrl);
   });
